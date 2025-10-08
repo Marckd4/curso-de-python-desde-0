56c13a8574fa0c19289f1589945f614f1cc40274
Marckd4 <mmartinezcanete@gmail.com>
2025-10-08 10:38:11 -0300

    basico02

--- 02-string.py
+++ 02-string.py
@@ -11,4 +11,9 @@ cosas basicas de python
 # argumento son los valores que son de una funcion 
 # colocar print de comienzo
 
-print(len(nombre_curso))
+print(len(nombre_curso)) # funcion de len permite tener la longuitud de cada texto
+print(nombre_curso[0]) # extrae el numero 
+print(nombre_curso[0:4]) # recortar cararcteres 
+print(nombre_curso[2:]) # extraer datos de izquierda o deraha y por defecto 
+print(nombre_curso[:5]) # extraer dererecha asume el valor por defecto.
+print(nombre_curso[:]) # extrae todo los datos por defecto de la variable 
---
+++ 03-format-strings.py
@@ -0,0 +1,10 @@
+
+# operador de concatenacion es la + 
+# formateo de string (f")
+nombre = "marcos" 
+apellido = "martinez"
+
+#nombre_completo = nombre + " " + apellido
+nombre_completo = f"{nombre} {apellido}"
+#nombre_completo =f"{nombre[0]} {2+5}" puede ser conjugado de distintos conceptos 
+print(nombre_completo)

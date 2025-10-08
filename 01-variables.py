46d1d29d9b812ab860f85d1ab7d661ea0bcdda65
Marckd4 <mmartinezcanete@gmail.com>
2025-10-08 10:19:39 -0300

    basico01

---
+++ 01-variables.py
@@ -0,0 +1,7 @@
+nombre_curso = "python" 
+print(nombre_curso)
+
+
+alumnos = 5000 # integrer
+puntaje = 9.9  # float 
+publicado = True #Boolean False - True 
---
+++ 02-string.py
@@ -0,0 +1,14 @@
+# string son variables de texto 
+
+nombre_curso = "Python"
+descripcion_curso = """  
+curso es necesario para aprender las 
+cosas basicas de python
+"""
+#print(nombre_curso, descripcion_curso)
+
+# funcion de len permite tener la longuitud de cada texto
+# argumento son los valores que son de una funcion 
+# colocar print de comienzo
+
+print(len(nombre_curso))
